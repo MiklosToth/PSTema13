702b2b3b603cf31295fe053eb3a4bbd8ecc76a07
Remus <Remus@Remus-TOSH>
2012-12-01 23:51:37 +0200

    Versiune schelet . . .

---
+++ src/Logic/Functionality.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Remus
+ */
+public class Functionality {
+
+    public static Spectacol[] getSpectacole(Date data) {
+        return null;
+    }
+
+    public static Bilete[] getBilete(Spectacol spectacol) {
+        return null;
+    }
+
+    public static Angajat[] getAngajati() {
+        return null;
+    }
+
+    public static Figurant[] getFiguranti() {
+        return null;
+    }
+
+    public static boolean introducereSpectacol(Spectacol spectacol) {
+        return true;
+    }
+    
+    public static boolean introducereBilete(){
+        
+    }
+}
